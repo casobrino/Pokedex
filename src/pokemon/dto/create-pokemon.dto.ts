89ee13103b2882647758375a25ca6b570c58e03a
Cesar Sobrino <casobrino_0@hotmail.com>
2022-12-17 01:51:10 -0600

    Pokedex created CRUD and Conexion with MongoDB with Docker

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,11 @@
+import { IsString, IsInt, MinLength, IsPositive, Min } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
