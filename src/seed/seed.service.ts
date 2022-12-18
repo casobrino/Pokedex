c54004585bfa336d6dad76875aa7d5e34c97b11d
Cesar Sobrino <casobrino_0@hotmail.com>
2022-12-17 18:18:02 -0600

    Axios customized adapter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,23 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
-import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
+
+import { PokeResponse } from './interfaces/poke-response.interface';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({}); //delete * from pokemons
 
-    const { data } = await this.axios.get<PokeResponse>(
+    const data = await this.http.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
@@ -32,8 +33,8 @@ export class SeedService {
       pokemonToInsert.push({ name, no }); //[{name: bulbasaur, no: 1}]
     });
 
-    await this.pokemonModel.insertMany(pokemonToInsert);
     //insert into pokemons(name. no)
+    await this.pokemonModel.insertMany(pokemonToInsert);
 
     return 'Seed Executed';
   }
